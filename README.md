b2c2068d6b2dac5210f8ed96908be7e09704c374
osh925 <mickeyosh925@gmail.com>
2024-12-07 22:50:28 +0900

    dbg: minor debugs

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # CurAItor
+좋아하는 음악과 어울리는 분위기의 예술작품 큐레이션 서비스 데모입니다.
+
+## Project Introduction
+`Qwen2-Audio-7B` 모델을 이용하여 오디오 파일로부터 음악의 분위기와 장르를 텍스트 형태로 얻습니다.
+
+그 텍스트를 `clip-vit-base-patch16` 모델에 태워서 예술작품 데이터베이스에서 가장 유사도가 높은 작품을 추천합니다.
+
+예술품 데이터베이스는 `wikiart` 데이터셋에서 랜덤하게 1000개를 샘플하여 사용하였습니다.
+
+## How to Run
+
+### Building Docker Image from Local
+
+## Expected Improvements
+`Qwen2-Audio-7B` 모델이 상당히 무겁고 inference가 느립니다. 단독으로 사용해 보면 음원파일의 물리적인 분석(조성, 템포 같은 것)에 상당히 집착하는 것으로 보이는데 task에 맞게 fine-tuning할 수 있다면 더 좋을 것입니다. 현 버전에서는 prompt engineering 수준에서 적당히 때우고 넘겼습니다. 
+
+원래 목표는 그림 띄워 주면서 작기 및 작품 이름 정도는 같이 알려주고 싶었는데, 안타깝게도 `wikiart` 데이터셋은 원래 비전 모델 트레이닝셋이라 작품 이름은 데이터베이스에 포함되어 있지 않았습니다. 일일이 labeling하기는 기말이 급했기 때문에 넘어가 주시면 고맙겠습니다...
--- frontend/app.py
+++ frontend/app.py
@@ -10,6 +10,8 @@ st.subheader("Fine Art Curation Based on Your Music Taste")
 # 파일 업로드 위젯
 uploaded_file = st.file_uploader("Upload an mp3 file", type=["mp3"])
 
+FASTAPI_URL = "http://127.0.0.1:8000/upload/"
+
 if uploaded_file is not None:
     st.audio(uploaded_file, format="audio/mp3")
 
@@ -17,7 +19,7 @@ if uploaded_file is not None:
         # File processing 진행 중 스피너 표시
         with st.spinner("Processing... This might take an eternity cuz underoptimized... fxxk final exam :("):
             files = {"file": (uploaded_file.name, uploaded_file.getvalue(), uploaded_file.type)}
-            response = requests.post("http://127.0.0.1:8000/upload/", files=files)
+            response = requests.post(FASTAPI_URL, files=files)
 
             if response.status_code == 200:
                 st.success("File processed successfully!")
